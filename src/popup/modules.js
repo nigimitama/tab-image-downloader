ee98f05a6dca07e5d21ff128b2b221f98019ea03
nigimitama <nigimitama16@gmail.com>
2025-03-10 00:00:23 +0900

    feat: support twitter

--- src/popup/modules.js
+++ src/popup/modules.js
@@ -4,56 +4,78 @@ export const getSyncData = async (keys) => {
     const items = await new Promise((resolve, reject) => {
       chrome.storage.sync.get(keys, (result) => {
         if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError.message);
+          reject(chrome.runtime.lastError.message)
         } else {
-          resolve(result);
+          resolve(result)
         }
-      });
-    });
-    return items;
+      })
+    })
+    return items
   } catch (error) {
-    console.error("Error retrieving data:", error);
-    throw error;
+    console.error("Error retrieving data:", error)
+    throw error
   }
-};
+}
 
 export const sleep = async (second) => {
   // 指定秒数待つ関数
-  return new Promise((resolve) => setTimeout(resolve, 1000 * second));
-};
+  return new Promise((resolve) => setTimeout(resolve, 1000 * second))
+}
 
 const isImageURL = (url) => {
-  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".svg"];
-  const u = new URL(url);
+  return isImageFormat(url) || isTwitterImage(url)
+}
+
+const isImageFormat = (url) => {
+  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".svg"]
+  const u = new URL(url)
 
   for (const ext of imageExtensions) {
     if (u.pathname.toLowerCase().endsWith(ext)) {
-      return true;
+      return true
     }
   }
+  return false
+}
 
-  return false;
-};
+const isTwitterImage = (url) => {
+  // twitterは https://pbs.twimg.com/media/Glfh8q2awAA8nXq?format=png&name=small のようになっている
+  const u = new URL(url)
+  const isTwitterMedia = u.host === "pbs.twimg.com" && u.pathname.startsWith("/media/")
+  if (!isTwitterMedia) return false
+
+  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg"]
+  for (const ext of imageExtensions) {
+    if (u.searchParams.get("format") === ext) return true
+  }
+  return false
+}
 
 export const getImageTabs = async () => {
   // tabs: Tab[]
-  const tabs = await chrome.tabs.query({ currentWindow: true });
-  const imageTabs = tabs.filter((tab) => isImageURL(tab.url));
-  console.log(`${imageTabs.length} image tabs found.`);
-  return imageTabs;
-};
+  const tabs = await chrome.tabs.query({ currentWindow: true })
+  const imageTabs = tabs.filter((tab) => isImageURL(tab.url))
+  console.log(`${imageTabs.length} image tabs found.`)
+  return imageTabs
+}
 
 export const downloadFile = (url, filename) => {
   const downloading = chrome.downloads.download({
     url: url,
     filename: filename,
     saveAs: false, // 保存先の選択ダイアログを表示しない
-  });
-  return downloading;
-};
+  })
+  return downloading
+}
 
 export const getFileName = (url) => {
-  const u = new URL(url);
-  const fileName = u.pathname.split("/").pop();
-  return fileName;
-};
+  const u = new URL(url)
+  const fileName = u.pathname.split("/").pop()
+
+  if (isTwitterImage(url)) {
+    const extension = `.${u.searchParams.get("format")}`
+    return `${fileName}${extension}`
+  }
+
+  return fileName
+}
